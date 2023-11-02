c99914aff3efc7ae02641efa074ce568b1a8d1e1
kllall145 <149675904+kllall145@users.noreply.github.com>
2023-11-02 15:23:17 +0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// require_once '../src/Test.php';
+
+require '../vendor/autoload.php';
+
+use Kllall145\Test;
+
+$test = new Test();
+$test->index();
---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kllall145;
+
+class Test
+{
+    public function index()
+    {
+        echo "test_for_packagist";
+    }
+}
